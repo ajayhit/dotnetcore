dae8e99250957fab3eb4ca4b3bc32b4fd96abe77
ajay kumar <ajayhit@hotmail.com>
2021-05-23 22:52:37 +0530

    work cookies

--- Areas/Users/Controllers/HomeController.cs
+++ Areas/Users/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApplication1.Areas.Users.Controllers
@@ -9,8 +14,37 @@ namespace WebApplication1.Areas.Users.Controllers
     [Area("Users")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            //var client = new RestClient("https://localhost:44347/api/User/refresh-token");
+            //client.Timeout = -1;
+            //var request = new RestRequest(Method.POST);
+            //IRestResponse response = client.Execute(request);
+            //dynamic resp = JsonConvert.DeserializeObject(response.Content);
+            //var isAuthenticated = resp.isAuthenticated;
+            //if(isAuthenticated==true)
+            //{
+
+            //}
+            //Console.WriteLine(response.Content);
+
+            CookieContainer cookieContainer = new CookieContainer();
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                CookieContainer = cookieContainer
+            };
+            handler.CookieContainer = cookieContainer;
+            var client = new HttpClient(handler);
+
+
+            var loginResponse = await client.PostAsync("https://localhost:44347/api/User/refresh-token", null);
+            var chk = loginResponse.Content.ReadAsStringAsync().Result;
+
+
+            var authCookie = cookieContainer.GetCookies(new Uri("https://localhost:44347/")).Cast<Cookie>().Single(cookie => cookie.Name == "refreshToken");
+            cookieContainer.SetCookies(new Uri("https://localhost:44347/"), "refreshToken=" + authCookie.Value);
+         //   return RedirectToAction("Index", "Home", new { Area = "Users" });
+
             return View();
         }
     }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using WebApplication1.Models;
 
@@ -21,6 +27,33 @@ namespace WebApplication1.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string useid,string pass)
+        {
+            CookieContainer cookieContainer = new CookieContainer();
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                CookieContainer = cookieContainer
+            };
+            handler.CookieContainer = cookieContainer;
+            var client = new HttpClient(handler);
+            var req = new
+            {
+                Email = "user@secureapi.com",
+                Password = "Pa$$w0rd."
+            };
+            var req1 = JsonConvert.SerializeObject(req);
+            var stringContent = new StringContent(req1, Encoding.UTF8, "application/json"); // use MediaTypeNames.Application.Json in Core 3.0+ and Standard 2.1+
+
+
+            var loginResponse =await  client.PostAsync("https://localhost:44347/api/User/token", stringContent);
+            var chk = loginResponse.Content.ReadAsStringAsync().Result; 
+
+
+            var authCookie = cookieContainer.GetCookies(new Uri("https://localhost:44347/")).Cast<Cookie>().Single(cookie => cookie.Name == "refreshToken");
+            cookieContainer.SetCookies(new Uri("https://localhost:44347/"), "refreshToken="+authCookie.Value);
+            return RedirectToAction("Index", "Home", new { Area = "Users" });
         }
 
         public IActionResult Privacy()
